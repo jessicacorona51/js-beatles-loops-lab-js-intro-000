a7d458c380406c5b66af0b4c3c7b06607aba33c0
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-12 22:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+function johnLennonFacts(facts) {
+    let i = 0
+    while(i < facts.length){
+      facts[i] = facts[i] + "!!!"
+      i++
+    }
+}
