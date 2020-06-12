12f2d0943f651f34b0cc4c1ca18190b2231efebb
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-12 22:28:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function johnLennonFacts() {
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"];
-  var array = []; 
+  var array = [];
   var i = 0;
   while (array.length < facts.length) {
     array.push(facts[i] + "!!!");
