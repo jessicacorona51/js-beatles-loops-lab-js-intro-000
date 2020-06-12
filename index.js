7223229ff117bd93b10a70c72a99919b27ea9ec8
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-12 22:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice","While on your, he enjpyed playing Monopoly", "Harmonica was the first instrument Lennon lerned to play"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
   while (array.length < facts.length) {
@@ -16,6 +16,7 @@ function johnLennonFacts() {
   }
   return array;
 }
+
 function iLoveTheBeatles(number) {
   var array = [];
   do {
