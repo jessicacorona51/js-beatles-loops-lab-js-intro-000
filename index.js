82307f2817b01b7c8c897e01c9716897f81ad096
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-12 22:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var = [];
+  for (var i = 0; i < musicians.length; i++) {
+    console.log (musicians[i] + "plays" + instruments[i]);
+  }
+  return array;
+}
